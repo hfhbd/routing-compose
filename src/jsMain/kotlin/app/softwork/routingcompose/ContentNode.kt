8d22f97d29326ac63e353dacf2f7abcdd8b05ab3
hfhbd <hfhbd@users.noreply.github.com>
2021-06-16 10:38:46 +0200

    Add documentation

--- src/jsMain/kotlin/app/softwork/routingcompose/ContentNode.kt
+++ src/jsMain/kotlin/app/softwork/routingcompose/ContentNode.kt
@@ -2,6 +2,9 @@ package app.softwork.routingcompose
 
 import androidx.compose.runtime.*
 
+/**
+ * [Node] holding a `@Composable` block, which will be displayed, when a route matches this [Node].
+ */
 public abstract class ContentNode : Node() {
     @Composable
     public abstract fun display(argument: String)
--- src/jsMain/kotlin/app/softwork/routingcompose/NavBuilder.kt
+++ src/jsMain/kotlin/app/softwork/routingcompose/NavBuilder.kt
@@ -3,8 +3,19 @@ package app.softwork.routingcompose
 import androidx.compose.runtime.*
 import kotlinx.uuid.*
 
+/**
+ * Use the DSL functions to build the expected route handled by a [Router].
+ * If two routes matches the same path, the first declared route is choosen.
+ *
+ * Unfortunately, every `@Composable` block has to be inside a [ContentNode].
+ */
 public class NavBuilder internal constructor(private val node: RouteNode) {
 
+    /**
+     * Executes its children when the requested subroute matches this constant [route].
+     *
+     * To match `foo/bar`, create a [route] inside the first [route].
+     */
     @RouteBuilderDSL
     public fun route(
         route: String,
@@ -15,6 +26,11 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
         node.children += childNode
     }
 
+    /**
+     * Executes its children when the requested subroute is a [String].
+     *
+     * To get the route parameter, call the [Lazy] parameter inside a [ContentNode].
+     */
     @RouteBuilderDSL
     public fun stringRoute(nestedRoute: NavBuilder.(Lazy<String>) -> Unit = {}) {
         val childNode = StringRouteNode()
@@ -26,6 +42,9 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
         node.children += childNode
     }
 
+    /**
+     * Displays this `@Composable` block when the last subroute is a [String].
+     */
     @ContentBuilderDSL
     public fun string(content: @Composable (String) -> Unit) {
         val childNode = StringContentNode().apply {
@@ -35,6 +54,11 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
     }
 
 
+    /**
+     * Executes its children when the requested subroute is a [Int].
+     *
+     * To get the route parameter, call the [Lazy] parameter inside a [ContentNode].
+     */
     @RouteBuilderDSL
     public fun intRoute(nestedRoute: NavBuilder.(Lazy<Int>) -> Unit = {}) {
         val childNode = IntRouteNode()
@@ -46,6 +70,9 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
         node.children += childNode
     }
 
+    /**
+     * Displays this `@Composable` block when the last subroute is a [Int].
+     */
     @ContentBuilderDSL
     public fun int(content: @Composable (Int) -> Unit) {
         val childNode = IntContentNode().apply {
@@ -54,6 +81,12 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
         node.children += childNode
     }
 
+
+    /**
+     * Executes its children when the requested subroute is a [UUID].
+     *
+     * To get the route parameter, call the [Lazy] parameter inside a [ContentNode].
+     */
     @RouteBuilderDSL
     public fun uuidRoute(nestedRoute: NavBuilder.(Lazy<UUID>) -> Unit = {}) {
         val childNode = UUIDRouteNode()
@@ -65,7 +98,9 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
         node.children += childNode
     }
 
-
+    /**
+     * Displays this `@Composable` block when the last subroute is a [UUID].
+     */
     @ContentBuilderDSL
     public fun uuid(content: @Composable (UUID) -> Unit) {
         val childNode = UUIDContentNode().apply {
@@ -74,7 +109,9 @@ public class NavBuilder internal constructor(private val node: RouteNode) {
         node.children += childNode
     }
 
-
+    /**
+     * Always displays this `@Composable` when called by [RouteNode.execute].
+     */
     @ContentBuilderDSL
     public fun noMatch(content: @Composable () -> Unit) {
         node.children += SimpleContentNode().apply { this.content = content }
