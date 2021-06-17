e8f4ad07d406039386ca1ddda9dc9ba4964dc002
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2021-06-16 22:52:42 -0700

    Added documentation and dev example of routing paths to index.html

--- src/jsMain/kotlin/app/softwork/routingcompose/BrowserRouter.kt
+++ src/jsMain/kotlin/app/softwork/routingcompose/BrowserRouter.kt
@@ -6,6 +6,15 @@ import kotlinx.browser.*
 
 /**
  * A router leveraging the History API (https://developer.mozilla.org/en-US/docs/Web/API/History).
+ *
+ * Using a BrowserRouter requires you to implement a catch-all to send the same resource for
+ * every path the router intends to control. BrowserRouter will handle the proper child composition.
+ *
+ * Without a catch-all rule, will get a 404 or "Cannot GET /path" error each time you refresh or
+ * request a specific path. Each server's implementation of a catch-all will be different and you
+ * should handle this based on the webserver environment you're running.
+ *
+ * For more information about this catch-all on
  */
 public object BrowserRouter : Router {
     private var subCounter = 0
