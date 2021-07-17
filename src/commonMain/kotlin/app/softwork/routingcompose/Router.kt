90d0326f00578c1184b0fae347ab0a50092c0dcb
hfhbd <hfhbd@users.noreply.github.com>
2021-07-17 15:22:32 +0200

    Remove deprecated RouterCompisitionLocal after wrong deprecation handling

--- src/commonMain/kotlin/app/softwork/routingcompose/Router.kt
+++ src/commonMain/kotlin/app/softwork/routingcompose/Router.kt
@@ -3,18 +3,7 @@ package app.softwork.routingcompose
 import androidx.compose.runtime.*
 import kotlin.reflect.*
 
-/**
- * Provide the router implementation through a CompositionLocal so deeper level
- * Composables in the composition can have access to the current router.
- *
- * This is particularly useful for [NavLink], so we can have a single Composable
- * agnostic of the top level router implementation.
- *
- * To use this composition, you need to invoke any [Router] implementation first.
- */
-@Deprecated("This will be internal again in the next release. Use val router = Router.current instead. Please create an issue, why you still need this.")
-public val RouterCompositionLocal: ProvidableCompositionLocal<Router> =
-    staticCompositionLocalOf { error("Router not defined, cannot provide through RouterCompositionLocal.") }
+
 
 public interface Router {
 
