5b38a88d2819fab36bce59da2920033a0a2a79d4
hfhbd <hfhbd@users.noreply.github.com>
2021-07-21 12:21:39 +0200

    Add MavenCentral publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import java.util.Base64
+
 plugins {
     kotlin("multiplatform") version "1.5.21"
     id("org.jetbrains.compose") version "0.5.0-build262"
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "app.softwork"
@@ -47,13 +51,58 @@ kotlin {
     }
 }
 
+task("emptyJar", Jar::class) { }
+
 publishing {
-    repositories {
-        maven(url = "https://maven.pkg.github.com/hfhbd/routing-compose") {
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+    publications.all {
+        if (this is MavenPublication) {
+            artifact(tasks.getByName("emptyJar")) {
+                classifier = "javadoc"
             }
+            pom {
+                name.set("app.softwork UUID Library")
+                description.set("A multiplatform Kotlin UUID library, forked from https://github.com/cy6erGn0m/kotlinx-uuid")
+                url.set("https://github.com/hfhbd/kotlinx-uuid")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hfhbd")
+                        name.set("Philip Wedemann")
+                        email.set("mybztg+mavencentral@icloud.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git://github.com/hfhbd/kotlinx-uuid.git")
+                    developerConnection.set("scm:git://github.com/hfhbd/kotlinx-uuid.git")
+                    url.set("https://github.com/hfhbd/kotlinx-uuid")
+                }
+            }
+        }
+    }
+}
+(System.getProperty("signing.privateKey") ?: System.getenv("SIGNING_PRIVATE_KEY"))?.let {
+    String(Base64.getDecoder().decode(it)).trim()
+}?.let { key ->
+    println("found key, config signing")
+    signing {
+        val signingPassword = System.getProperty("signing.password") ?: System.getenv("SIGNING_PASSWORD")
+        useInMemoryPgpKeys(key, signingPassword)
+        sign(publishing.publications)
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getProperty("sonartype.apiKey") ?: System.getenv("SONARTYPE_APIKEY"))
+            password.set(System.getProperty("sonartype.apiToken") ?: System.getenv("SONARTYPE_APITOKEN"))
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
     }
 }
