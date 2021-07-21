6301d3efa7458b10b6841448d6f6ad6723001067
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-07-21 12:25:46 +0200

    Add MavenCentral publishing (#55)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- integrationTest/build.gradle.kts
+++ integrationTest/build.gradle.kts
@@ -6,12 +6,6 @@ plugins {
 repositories {
     mavenCentral()
     maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    maven(url = "https://maven.pkg.github.com/hfhbd/*") {
-        credentials {
-            username = System.getProperty("gpr.user") ?: System.getenv("GITHUB_ACTOR")
-            password = System.getProperty("gpr.key") ?: System.getenv("GITHUB_TOKEN")
-        }
-    }
 }
 
 kotlin {
