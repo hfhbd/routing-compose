8aa4d2de5916bbb51d56322f0c6ec674f47270eb
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-03-09 19:53:22 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import io.gitlab.arturbosch.detekt.*
 
 plugins {
     kotlin("multiplatform") version "1.8.10"
-    id("org.jetbrains.compose") version "1.4.0-alpha01-dev968"
+    id("org.jetbrains.compose") version "1.3.1"
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
