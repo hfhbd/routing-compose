29f171a3c1183d17f17c28ac60f6793ac8e342f8
hfhbd <hfhbd@users.noreply.github.com>
2023-04-10 19:31:35 +0200

    Add build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,21 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version "3.12.6"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI") != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
+}
+
 rootProject.name = "routing-compose"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
