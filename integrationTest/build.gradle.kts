04fcc59f73a3d1cdc98789e97414a184780d7626
hfhbd <hfhbd@users.noreply.github.com>
2024-02-04 11:33:18 +0100

    Bump Compose to 1.6.0-beta01

--- integrationTest/build.gradle.kts
+++ integrationTest/build.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
         commonMain {
             dependencies {
                 api(projects.routingCompose)
+                api(libs.kotlinx.coroutines.core)
             }
         }
         named("jvmMain") {
