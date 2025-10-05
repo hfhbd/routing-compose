2f23ea8aad2d0baca0b047f1e3515d23e8e2f49d
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-10-05 11:58:49 +0200

    Update Kotlin to 2.2.20 and compose to 1.9.0

--- integrationTest/src/wasmJsMain/kotlin/main.kt
+++ integrationTest/src/wasmJsMain/kotlin/main.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.window.ComposeViewport
 import app.softwork.routingcompose.*
 
 @ExperimentalComposeUiApi
-fun main() = ComposeViewport("WASM Test") {
+fun main() = ComposeViewport {
     var wasMagic42 by remember { mutableStateOf(false) }
     HashRouter("/1") {
         Demo(1, wasMagic42, { wasMagic42 = it })
